641e4a3c15069ed22f9930a29e61d8e16f473db1
MimonN <m.a.nedilsky@gmail.com>
2023-03-04 16:10:14 -0500

    Fetch MenuItems and create first Interface

--- src/Container/App.tsx
+++ src/Container/App.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import { Footer, Header } from '../Components/Layout';
+import { useState, useEffect } from 'react';
+import { menuItemModel } from '../Interfaces';
 
 function App() {
+  const [menuItems, setMenuItems] = useState<menuItemModel[]>([]);
+
+  useEffect(() => {
+    fetch('https://localhost:7054/api/MenuItem')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setMenuItems(data.result);
+      });
+  }, []);
+
   return (
-    <div><Header />Mani Component<Footer /></div>
+    <div>
+      <Header />
+      Mani Component
+      <Footer />
+    </div>
   );
 }
 
