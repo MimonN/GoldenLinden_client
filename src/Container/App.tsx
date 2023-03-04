60cbe0b1fd1782be5cca207af5e0a7d1469029fc
MimonN <m.a.nedilsky@gmail.com>
2023-03-04 17:50:01 -0500

    Card Component

--- src/Container/App.tsx
+++ src/Container/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
 import { Footer, Header } from '../Components/Layout';
-import { useState, useEffect } from 'react';
-import { menuItemModel } from '../Interfaces';
+import { Home } from '../Pages';
+
 
 function App() {
-  const [menuItems, setMenuItems] = useState<menuItemModel[]>([]);
-
-  useEffect(() => {
-    fetch('https://localhost:7054/api/MenuItem')
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setMenuItems(data.result);
-      });
-  }, []);
+  
 
   return (
     <div>
       <Header />
-      Mani Component
+      <Home />
       <Footer />
     </div>
   );
