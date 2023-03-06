87dcffa3b159c96af36308360d6785d9ba42a4cf
MimonN <m.a.nedilsky@gmail.com>
2023-03-05 20:57:20 -0500

    Get Shopping Cart and Store in Slice

--- src/Container/App.tsx
+++ src/Container/App.tsx
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Footer, Header } from '../Components/Layout';
 import { Home, MenuItemDetails, NotFound } from '../Pages';
-
+import { useDispatch } from 'react-redux';
+import { useGetShoppingCartQuery } from '../Apis/shoppingCartApi';
+import { setShoppingCart } from '../Storage/Redux/shoppingCartSlice';
 
 function App() {
-  
+  const dispatch = useDispatch();
+
+  const { data, isLoading } = useGetShoppingCartQuery(
+    'c8511786-4f45-4bfa-8616-abb7813ce6b0'
+  );
+
+  useEffect(() => {
+    if (!isLoading) {
+      console.log(data.result);
+      dispatch(setShoppingCart(data.result?.cartItems));
+    }
+  }, [data]);
 
   return (
     <div>
       <Header />
-      <div className='pb-5'>
+      <div className="pb-5">
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='menuItemDetails/:menuItemId' element={<MenuItemDetails />} />
-          <Route path='*' element={<NotFound />} />
+          <Route path="/" element={<Home />} />
+          <Route
+            path="menuItemDetails/:menuItemId"
+            element={<MenuItemDetails />}
+          />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
       <Footer />
