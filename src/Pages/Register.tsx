1f2ded6d70c5754268c7e90c958e212a6bc912cc
MimonN <m.a.nedilsky@gmail.com>
2023-03-06 14:04:54 -0500

    making Register Page Controlled Component

--- src/Pages/Register.tsx
+++ src/Pages/Register.tsx
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { inputHelper } from '../Helper';
+import { SD_Roles } from '../Utility/SD';
 
 function Register() {
+  const [loading, setLoading] = useState(false);
+  const [userInput, setUserInput] = useState({
+    userName: '',
+    password: '',
+    role: '',
+    name: '',
+  });
+
+  const handleUserInput = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const tempData = inputHelper(e, userInput);
+    setUserInput(tempData);
+  };
+
   return (
     <div className="container text-center">
       <form method="post">
@@ -12,6 +29,9 @@ function Register() {
               className="form-control"
               placeholder="Enter Username"
               required
+              value={userInput.userName}
+              onChange={handleUserInput}
+              name="userName"
             />
           </div>
           <div className="col-sm-6 offset-sm-3 col-xs-12 mt-4">
@@ -20,6 +40,9 @@ function Register() {
               className="form-control"
               placeholder="Enter Name"
               required
+              value={userInput.name}
+              onChange={handleUserInput}
+              name="name"
             />
           </div>
           <div className="col-sm-6 offset-sm-3 col-xs-12 mt-4">
@@ -28,13 +51,22 @@ function Register() {
               className="form-control"
               placeholder="Enter Password"
               required
+              value={userInput.password}
+              onChange={handleUserInput}
+              name="password"
             />
           </div>
           <div className="col-sm-6 offset-sm-3 col-xs-12 mt-4">
-            <select className="form-control form-select" required>
+            <select
+              className="form-control form-select"
+              required
+              value={userInput.role}
+              onChange={handleUserInput}
+              name="role"
+            >
               <option value="">--Select Role--</option>
-              <option value="customer">Customer</option>
-              <option value="admin">Admin</option>
+              <option value={`${SD_Roles.CUSTOMER}`}>Customer</option>
+              <option value={`${SD_Roles.ADMIN}`}>Admin</option>
             </select>
           </div>
         </div>
