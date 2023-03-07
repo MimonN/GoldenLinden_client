1b6522c90dff9911efe8af821d6e6b0286887608
MimonN <m.a.nedilsky@gmail.com>
2023-03-07 12:59:53 -0500

    Stripe Cart Demo

--- src/Pages/Payment.tsx
+++ src/Pages/Payment.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import { PaymentForm } from '../Components/Page/Payment';
 
 function Payment() {
   const {
     state: { apiResult, userInput },
   } = useLocation();
-  console.log(apiResult);
-  console.log(userInput);
-  return <div>Payment</div>;
+
+  const stripePromise = loadStripe(
+    'pk_test_51MhZrbLunxQjCbLSJ4fWmoD7UCoWvzO1hh5NsWbZYdapCR809D8bArdmnQMLk5NKbjjPOIfrriFObQLCU3QmDxmF00coOmUHka'
+  );
+  const options = {
+    // passing the client secret obtained from the server
+    clientSecret: apiResult.clientSecret,
+  };
+  return (
+    <Elements stripe={stripePromise} options={options}>
+      <PaymentForm />
+    </Elements>
+  );
 }
 
 export default Payment;
